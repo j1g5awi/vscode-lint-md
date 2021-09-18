246d8745ad30daa025b6d0263d0878f51f5c008d
Jigsaw <j1g5aw@foxmail.com>
2021-09-18 20:14:27 +0800

    :wrench: Add configuration for lint-md

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,15 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { fix } from '@lint-md/core';
+import { fix, LintMdRulesConfig } from '@lint-md/core';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	vscode.languages.registerDocumentFormattingEditProvider('markdown', {
 		provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
-			const fixed = fix(document.getText());
+			const rules: LintMdRulesConfig = vscode.workspace.getConfiguration('lint-md').get('rules')
+			const fixed = fix(document.getText(), rules);
 			const start = new vscode.Position(0, 0);
 			const end = document.lineAt(document.lineCount - 1).range.end;
 			return [vscode.TextEdit.replace(new vscode.Range(start, end), fixed)];
